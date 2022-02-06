2d363ba2610cf755afd6f5abea7792758a83cfb0
Bhavesh Wadhwani <bhaveshwadhwani21@gmail.com>
2022-02-06 18:58:07 +0530

    ticket service done

--- common/src/middlewares/error-handler.ts
+++ common/src/middlewares/error-handler.ts
@@ -10,7 +10,7 @@ export const errorHandler = (
   if (err instanceof CustomError) {
     return res.status(err.statusCode).send({ errors: err.serializeErrors() });
   }
-
+  console.error(err);
   res.status(400).send({
     errors: [{ message: "Something went wrong" }],
   });
