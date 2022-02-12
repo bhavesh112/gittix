2a23eb2e134b4ef9913887287f58b9ecc3120f61
Bhavesh Wadhwani <bhaveshwadhwani21@gmail.com>
2022-02-12 11:15:44 +0530

    added nats

--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -7,6 +7,7 @@ import jwt from "jsonwebtoken";
 declare global {
   var signin: () => string[];
 }
+jest.mock("../nats-wrapper");
 let mongo: any;
 beforeAll(async () => {
   process.env.JWT_KEY = "asdfasdf";
